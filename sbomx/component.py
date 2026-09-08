7134598195fb4fd78c1ff121a47379810e802f0d
Akhil <spider20436@gmail.com>
2026-09-08 12:53:01 +0530

    feat: create components from dependencies

--- sbomx/component.py
+++ sbomx/component.py
@@ -6,3 +6,11 @@ class Component:
     name: str
     version: str
     ecosystem : str
+
+    @classmethod #let us create [Componenet] from another piece of data.{the other data is parsed}
+    def from_dependency(cls, dependency, ecosystem): #receives {parsed dependency dictionary}, {eg. "python or "node}
+        return cls( #creates a new component, {cls reefers to the "Component" class itself}
+            name = dependency["name"], #gets the package name
+            version=dependency["version"], #version
+            ecosystem=ecosystem #ecosystem we detected
+        )
